068ffe8baee7a281d9f9314ea8983b211be6164c
Joseph Ampadu <jkampadujnr@gmail.com>
2023-04-03 10:04:59 +0000

    Remove Rails project files

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => "/api-docs"
-  mount Rswag::Api::Engine => "/api-docs"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
