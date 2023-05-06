f52094d6edec924048fc26490a8df8f501af2223
Kingsley Segu Acquah <75037978+Acquah-Kingsley-Segu@users.noreply.github.com>
2023-05-07 00:38:27 +0200

    Mongodb connection (#5)
    * Connect to MongoDB Atlas using mongoose and made app listen if and only if DB connects succesfully
    
    * Mongoose package addition
    
    * Directs how one should connect to the MongoDB and optional port provision
    
    * Show how to setup project

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 
@@ -6,8 +7,14 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
+// db connection
+mongoose.connect(`${process.env.MONGO_CONNECTION_STRING}`)
+  .then(() => {
+    console.log("DB connected")
+    app.listen(port, () => console.log(`[server]: Started on port ${port}`));
+  })
+  .catch((err) => console.log(err));
+
 app.get("/", (req: Request, res: Response) =>
   res.send("Express + TypeScript Server")
 );
-
-app.listen(port, () => console.log(`[server]: Started on port ${port}`));
